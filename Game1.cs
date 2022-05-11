2339f23a6b55de935c2d3c65327bbd11450cfeab
dave2011 <dave2011@5CD9011VMT>
2022-05-11 14:04:13 +0200

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,145 @@
+﻿using CookieClicker.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CookieClicker
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private List<MyComponent> _gameComponents;
+
+        
+        Texture2D CookieSprite;
+        Texture2D BackgroundSprite;
+        Texture2D StoreSprite;
+        SpriteFont gameFont;
+
+        Vector2 Cookieposition = new Vector2(420, 450);
+        const int CookieRadius = 300;
+        
+        MouseState mState;
+        bool mReleased = true;
+        int Cookies = 0;
+        int Click = 1;
+
+
+
+
+
+
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1600;
+            _graphics.PreferredBackBufferHeight = 900;
+            _graphics.ApplyChanges();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var cursorButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/galleryFont"))
+            {
+                Position = new Vector2(1115, 200),
+                Text = "Cursor",
+            };
+
+            cursorButton.click += CursorButton_click;
+
+            var grandmaButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/galleryFont"))
+            {
+                Position = new Vector2(1115, 285),
+                Text = "Grandma",
+            };
+
+            grandmaButton.click += GrandmaButton_click;
+
+            _gameComponents = new List<MyComponent>()
+            {
+               cursorButton,
+               grandmaButton,
+            };
+
+            StoreSprite = Content.Load<Texture2D>("Store");
+            CookieSprite = Content.Load<Texture2D>("Cookie");
+            BackgroundSprite = Content.Load<Texture2D>("Background");
+            gameFont = Content.Load<SpriteFont>("Fonts/galleryFont");
+           
+        }
+
+        private void GrandmaButton_click(object sender, System.EventArgs e)
+        {
+            if (Cookies >= 5)
+            {
+                Click *= 2;
+            }
+        }
+
+        private void CursorButton_click(object sender, System.EventArgs e)
+        {
+            Cookies += Click;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            mState = Mouse.GetState();
+
+            if (mState.LeftButton == ButtonState.Pressed && mReleased == true)
+            {
+                float mouseCookieDist = Vector2.Distance(Cookieposition, mState.Position.ToVector2());
+                if(mouseCookieDist < CookieRadius)
+                {
+                    Cookies += Click;
+                }
+                mReleased = false;
+            }
+
+            if (mState.LeftButton == ButtonState.Released)
+            {
+                mReleased = true;
+            }
+
+            foreach (var component in _gameComponents)
+                component.Update(gameTime);
+
+
+            base.Update(gameTime);
+        }
+        
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(BackgroundSprite, new Vector2(0, 0), Color.White);
+            _spriteBatch.DrawString(gameFont, "Cookies:" + " " + Cookies.ToString(), new Vector2(50, 50), Color.White);
+            _spriteBatch.Draw(CookieSprite, new Vector2(Cookieposition.X - CookieRadius, Cookieposition.Y - CookieRadius), Color.White); 
+            _spriteBatch.Draw(StoreSprite, new Vector2(1110, 0), Color.White);
+            foreach (var component in _gameComponents)
+                component.Draw(gameTime, _spriteBatch);
+                _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
